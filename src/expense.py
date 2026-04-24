8fcdcdba9e5ccd18d1202982cfb0202ce576dc26
Lance Tumabiene <altumabiene@my.cspc.edu.ph>
2026-04-24 09:09:56 +0800

    Update expense.py

--- src/expense.py
+++ src/expense.py
@@ -1,1 +1,16 @@
+class Expense:
+  def __init__(self, title: str, amount: float, category: str):
+    self.title = title
+    self.amount = amount
+    self.category = category
 
+  def to_dict(self):
+    return {
+      "title: self.title",
+      "amount: self.amount",
+      "category: self.category
+    }
+
+@staticmethod
+def from_dict(data: dict):
+  return Expense(data["title"], data["amount"], data["category"])
