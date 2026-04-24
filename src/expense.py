361c5fc2e21721d2fa28659a37c7aa2122a17398
Lance Tumabiene <altumabiene@my.cspc.edu.ph>
2026-04-24 15:55:26 +0800

    Update expense.py

--- src/expense.py
+++ src/expense.py
@@ -1,16 +1,26 @@
 class Expense:
-  def __init__(self, title: str, amount: float, category: str):
-    self.title = title
-    self.amount = amount
-    self.category = category
+    def __init__(self, id, title, amount, category, date):
+        self.id = id
+        self.title = title
+        self.amount = float(amount)
+        self.category = category
+        self.date = date
 
-  def to_dict(self):
-    return {
-      "title: self.title",
-      "amount: self.amount",
-      "category: self.category
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "amount": self.amount,
+            "category": self.category,
+            "date": self.date
+        }
 
-@staticmethod
-def from_dict(data: dict):
-  return Expense(data["title"], data["amount"], data["category"])
+    @staticmethod
+    def from_dict(data):
+        return Expense(
+            data["id"],
+            data["title"],
+            data["amount"],
+            data["category"],
+            data["date"]
+        )
